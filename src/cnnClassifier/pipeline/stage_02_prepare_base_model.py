ccbda941ac13a3d89d676c2a5200fa3869afa250
Francisco Carmona <jcarmfran@gmail.com>
2024-03-08 11:39:20 -0600

    Adding callback preparation

--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -9,6 +9,7 @@ class PrepareBaseModelTrainingPipeline:
     def __init__(self):
         pass
 
+
     def main(self):
         config = ConfigurationManager()
         prepare_base_model_config = config.get_prepare_base_model_config()
